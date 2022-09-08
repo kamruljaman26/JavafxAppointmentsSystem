dbc46e7fc802cd8a4ff36e25533da2f89357e6e6
Saif <kamruljaman@Kamruls-MacBook-Pro.local>
2022-09-09 02:12:06 +0600

    created hibernate database config and connection

---
+++ src/main/java/com/appoinments/system/utils/HibernateUtil.java
@@ -0,0 +1,34 @@
+package com.appoinments.system.utils;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+
+    private static SessionFactory buildSessionFactory() {
+        try {
+            // Create the SessionFactory from hibernate.cfg.xml
+            return new Configuration().configure().buildSessionFactory();
+        } catch (Throwable ex) {
+            // Make sure you log the exception, as it might be swallowed
+            System.err.println("Initial SessionFactory creation failed." + ex);
+            ex.printStackTrace();
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public synchronized static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+    public static void shutdown() {
+        // Close caches and connection pools
+        getSessionFactory().close();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getSessionFactory());
+    }
+}
